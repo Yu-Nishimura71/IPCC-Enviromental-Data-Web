97610da7bdd96d710e98c17ff8b922a76680b15a
Yu_Nishimura <nisyy003@mymail.unisa.edu.au>
2023-10-19 14:29:42 +1030

    Adding and declaring Country route

---
+++ Assig2/a2reactapp/src/routes/Country.js
@@ -0,0 +1,12 @@
+const Country = ({ }) => {
+    return (
+        <div>
+            <h2 className="text-center">
+                Country Lists
+            </h2>
+            <hr />
+        </div>
+    )
+}
+
+export default Country
