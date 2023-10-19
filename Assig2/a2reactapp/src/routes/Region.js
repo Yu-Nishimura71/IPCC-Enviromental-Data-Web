c1df47235bef0b588ff82bccd3e68d29c2ded944
Yu_Nishimura <nisyy003@mymail.unisa.edu.au>
2023-10-19 14:21:30 +1030

    Adding and declaring Region route

---
+++ Assig2/a2reactapp/src/routes/Region.js
@@ -0,0 +1,12 @@
+const Region = ({ }) => { 
+    return (
+        <div>
+            <h2 className="text-center">
+                Region Lists
+            </h2>
+            <hr />
+        </div>
+    )
+}
+
+export default Region
