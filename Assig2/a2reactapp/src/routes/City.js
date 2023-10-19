e534cba6b3218b53233562613ff99733400329b0
Yu_Nishimura <nisyy003@mymail.unisa.edu.au>
2023-10-19 14:30:23 +1030

    Adding and declaring City route

---
+++ Assig2/a2reactapp/src/routes/City.js
@@ -0,0 +1,12 @@
+const City = ({ }) => {
+    return (
+        <div>
+            <h2 className="text-center">
+                City Lists
+            </h2>
+            <hr />
+        </div>
+    )
+}
+
+export default City
