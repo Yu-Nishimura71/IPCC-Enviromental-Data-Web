060e802b185ad8b4456292709ae3fbc6ae1baeef
Yu_Nishimura <nisyy003@mymail.unisa.edu.au>
2023-10-19 14:34:06 +1030

    Adding and declaring Air Quality Data route

--- Assig2/a2reactapp/src/routes/AirQualData.js
+++ Assig2/a2reactapp/src/routes/AirQualData.js
@@ -1,1 +1,12 @@
-﻿
+const AirQualData = ({ }) => {
+    return (
+        <div>
+            <h2 className="text-center">
+                Air Quality Data
+            </h2>
+            <hr />
+        </div>
+    )
+}
+
+export default AirQualData
