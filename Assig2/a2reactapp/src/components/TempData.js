abf4aaa7b4cf8130489719b45ddef920a3e9fbfe
Yu_Nishimura <nisyy003@mymail.unisa.edu.au>
2023-10-23 16:24:15 +1030

    create fetch request function in the TempData

--- Assig2/a2reactapp/src/components/TempData.js
+++ Assig2/a2reactapp/src/components/TempData.js
@@ -1,8 +1,27 @@
 import React, { useState, useEffect } from "react";
 
 function TempData() {
+
     const [tempData, setTemp] = useState({});
     const [rawData, setRaw] = useState({ rawTemperatureData: [] });
+    const [countryId, setCountryID] = useState('');
+
+    useEffect(() => {
+        fetch(`http://localhost:5256/api/B_Countries/CountryTemperatureDetail/${countryId}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log("data", data);
+                setTemp(data);
+                setRaw(data.rawTemperatureData)
+                console.log("raw data", data.rawTemperatureData);
+            })
+    }, [countryId]);
+
+    return (
+        <div>
+            
+        </div>
+    )
 }
 
 export default TempData
