fe6c1dd6cab9132c6f64694a86642d52a028234e
Yu_Nishimura <nisyy003@mymail.unisa.edu.au>
2023-10-23 16:47:13 +1030

    add data table headers

--- Assig2/a2reactapp/src/components/TempCard.js
+++ Assig2/a2reactapp/src/components/TempCard.js
@@ -1,5 +1,25 @@
-const TempCard = () => {
-
+const TempCard = (maxYear, minYear, rawData) => {
+    return (
+        <div className="card">
+            <div className="card-header">
+                Temperature Data from {minYear} to{maxYear}
+            </div>
+            <div className="card-body">
+                <thead>
+                    <tr>
+                        <th>Year</th>
+                        <th>Country Name</th>
+                        <th>Unit</th>
+                        <th>Change</th>
+                        <th>Value</th>
+                        <th>Regional Avg</th>
+                        <th>Regional Min</th>
+                        <th>Regional Max</th>
+                    </tr>
+                </thead>
+            </div>
+        </div>
+    )
 }
 
 export default TempCard
